ca5bfd694bd3e991452011ba64e987e42dc8ed46
David E. Shere <david.e.shere@gmail.com>
2013-02-26 23:08:05 -0800

    Added binding for graphics_get_display_size.

--- src/VideoCore.hs
+++ src/VideoCore.hs
@@ -3,5 +3,20 @@ module VideoCore where
 import qualified VideoCore.Core as VC
 import qualified VideoCore.Core.EGL as EGL
 
-initialize :: IO ()
-initialize = VC.bcm_host_init
+import Foreign
+import Foreign.C
+
+bcmHostInit :: IO ()
+bcmHostInit = VC.bcm_host_init
+
+bcmHostDeinit :: IO ()
+bcmHostDeinit = VC.bcm_host_deinit
+
+graphicsGetDisplaySize :: CUShort -> IO (CInt, CUInt, CUInt)
+graphicsGetDisplaySize dispNum = 
+    alloca $ \wP ->
+    alloca $ \hP -> do
+        ret    <- VC.graphics_get_display_size dispNum wP hP
+        width  <- peek wP
+        height <- peek hP
+        return (ret, width, height)
--- src/VideoCore/Core.hs
+++ src/VideoCore/Core.hs
@@ -2,6 +2,13 @@
 module VideoCore.Core where
 
 import Foreign
---import Foreign.C
+import Foreign.C
 
-foreign import ccall unsafe "bcm_host_init" bcm_host_init :: IO ()
+foreign import ccall unsafe "bcm_host_init"
+  bcm_host_init :: IO ()
+
+foreign import ccall unsafe "bcm_host_deinit"
+  bcm_host_deinit :: IO ()
+
+foreign import ccall unsafe "graphics_get_display_size"
+  graphics_get_display_size :: CUShort -> Ptr CUInt -> Ptr CUInt -> IO CInt
